7c7e65dd6e11620b3c00ce1ef471664b7d36ae6c
Mahi Satyanarayana <msatya@appriver.com>
2020-05-26 00:14:02 -0500

    add and consume another package

--- helloworld/entry/entry.go
+++ helloworld/entry/entry.go
@@ -1,12 +1,16 @@
+//great blog on how to structure go applications - https://eli.thegreenplace.net/2019/simple-go-project-layout-with-modules/
 package main
 
 import (
 	"fmt"
 
 	"mahisatya.dev/hello"
+	"mahisatya.dev/hello/justhello"
 )
 
 func main() {
 	var result = hello.Hello()
 	fmt.Println(result)
+
+	fmt.Println(justhello.Hello())
 }
---
+++ helloworld/justhello/justhello.go
@@ -0,0 +1,7 @@
+package justhello
+
+
+// Hello is ...
+func Hello() string {
+	return "Hello, there. I ain't got no package."
+}
